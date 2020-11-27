176c5adcf00aeab336b1fcdb902c70314bb915db
banalna <judas.priest999@gmail.com>
2020-11-27 11:51:21 +0300

    fix links in docs

--- src/build/DefaultMongooseFactory.ts
+++ src/build/DefaultMongooseFactory.ts
@@ -7,7 +7,7 @@ import { MongooseConnection } from '../persistence/MongooseConnection';
 /**
  * Creates Mongoose components by their descriptors.
  * 
- * @see [[https://rawgit.com/pip-services-node/pip-services3-components-node/master/doc/api/classes/build.factory.html Factory]]
+ * @see [[https://pip-services3-node.github.io/pip-services3-components-node/classes/build.factory.html Factory]]
  * @see [[MongooseConnection]]
  */
 export class DefaultMongooseFactory extends Factory {
